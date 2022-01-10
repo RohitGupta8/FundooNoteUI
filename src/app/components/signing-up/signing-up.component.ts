6e756207ee99e48a6ff4593c32d6e18c1e0ccade
RohitGupta8 <therohitg@gmail.com>
2022-01-10 20:09:43 +0530

    initial commit.

---
+++ src/app/components/signing-up/signing-up.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-signing-up',
+  templateUrl: './signing-up.component.html',
+  styleUrls: ['./signing-up.component.scss']
+})
+export class SigningUpComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
